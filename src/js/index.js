027f7c01d9a46370a89cd448f14e5c20824e1f3a
Bashar Harfoush <bashar.harfoush@gmail.com>
2019-01-07 18:55:56 -0500

    webpack stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+require("../scss/main.scss");
 
 var markdown = require('markdown').markdown;
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,31 @@ const path = require('path');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: 'index.dist.js',
-    path: path.resolve(__dirname, 'public/js')
+    filename: 'js/index.dist.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+          }
+        }]
+      }
+    ]
   }
 };
